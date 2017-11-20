f54d030f410eddbfc8b972fac0bcd7c475fc18f6
raeeschachar <raees.chachar@arbisoft.com>
2017-11-20 23:31:18 +0500

    Like button with ajax

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
-from .models import Treasure
-from .forms import TreasureForm
+from django.http import HttpResponseRedirect, HttpResponse
+from .models import Treasure, User
+from .forms import TreasureForm, LoginForm
+from django.contrib.auth import authenticate, login, logout
 
 
 # Create your views here.
@@ -17,7 +18,53 @@ def detail(request, treasure_id):
 
 
 def post_treasure(request):
-    form = TreasureForm(request.POST)
+    form = TreasureForm(request.POST, request.FILES)
     if form.is_valid():
-        form.save(commit=True)
+        treasure = form.save(commit=False)
+        treasure.user = request.user
     return HttpResponseRedirect('/')
+
+
+def profile(request, username):
+    user = User.objects.get(username=username)
+    treasures = Treasure.objects.filter(user=user)
+    return render(request, 'profile.html', {'username': username, 'treasures': treasures})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            u = form.cleaned_data['username']
+            p = form.cleaned_data['password']
+            user = authenticate(username=u, password=p)
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponseRedirect('/')
+                else:
+                    print("The account has been disabled")
+            else:
+                print("The username and password were incorrect")
+    else:
+        form = LoginForm()
+        return render(request, 'login.html', {'form': form})
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect('/')
+
+
+def like_treasure(request):
+    treasure_id = request.GET.get('treasure_id', None)
+
+    likes = 0
+    if treasure_id:
+        treasure = Treasure.objects.get(id=int(treasure_id))
+        if treasure is not None:
+            likes = treasure.like + 1
+            treasure.like = likes
+            treasure.save()
+
+        return HttpResponse(likes)
