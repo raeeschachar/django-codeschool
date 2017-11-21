b5e8d3faac6bf30b199b5eb2a6b28a8fe86bdc7a
raeeschachar <raees.chachar@arbisoft.com>
2017-11-21 22:34:02 +0500

    ye lo

--- main_app/static/js/main.js
+++ main_app/static/js/main.js
@@ -14,3 +14,33 @@ $('button').on('click', function(event){
     });
 
 });
+
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie != '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            //Does this cookie  string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                cookieValue = decodeURIComponent(
+                    cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+}
+
+var csrftoken = getCookie('csrftoken');
+
+function csrfSafeMethod(method) {
+    //these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+$.ajaxSetup({
+    beforeSend: function(xhr, settings) {
+        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+        }
+    }
+});
